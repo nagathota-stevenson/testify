1405e31734b438172d3072e7ca8aa5d42347b557
nagathota.stevenson <nagathota.stevenson@gmail.com>
2024-09-10 11:28:08 -0400

    chore: disable notification

--- app/context/NotificationContext.js
+++ app/context/NotificationContext.js
@@ -6,7 +6,6 @@ import {
   collection,
   query,
   where,
-  Timestamp,
 } from "firebase/firestore";
 import { db } from "../firebase/config"; // Adjust the path to your Firebase config
 import { AuthContext } from "../context/AuthContext"; // Import AuthContext for user information
@@ -18,66 +17,90 @@ export const NotificationsProvider = ({ children }) => {
   const [notifications, setNotifications] = useState([]);
   const { user } = useContext(AuthContext);
   const audioRef = useRef(null);
+  const unsubscribeRef = useRef(null); // Holds the unsubscribe function
 
-  useEffect(() => {
-    if (user) {
-      const requestsQuery = query(
-        collection(db, "requests"),
-        where("uid", "==", user.uid)
-      );
-      const testimoniesQuery = query(
-        collection(db, "testimonies"),
-        where("uid", "==", user.uid)
-      );
+  // const handleSnapshot = () => {
+  //   if (user) {
+  //     const requestsQuery = query(
+  //       collection(db, "requests"),
+  //       where("uid", "==", user.uid)
+  //     );
 
-      const unsubscribeRequests = onSnapshot(requestsQuery, (snapshot) => {
-        const newRequests = snapshot.docs.map((doc) => ({
-          docId: doc.id,
-          prayers: doc.data().prayers,
-          type: "requests",
-        }));
+  //     // Listening to the snapshot
+  //     unsubscribeRef.current = onSnapshot(requestsQuery, (snapshot) => {
+  //       const newRequests = snapshot.docs.map((doc) => ({
+  //         docId: doc.id,
+  //         prayers: doc.data().prayers,
+  //         type: "requests",
+  //       }));
 
-        // Filter out prayers where isViewed is false
-        const newNotifications = newRequests.flatMap((request) =>
-          request.prayers
-            .filter((prayer) => !prayer.isViewed) // Filter out viewed prayers
-            .map((prayer) => ({
-              docId: request.docId,
-              uid: prayer.uid,
-              notificationTimestamp: prayer.time,
-              type: "requests",
-            }))
-        );
+  //       // Filter out prayers where isViewed is false
+  //       const newNotifications = newRequests.flatMap((request) =>
+  //         request.prayers
+  //           .filter((prayer) => !prayer.isViewed) // Filter out viewed prayers
+  //           .map((prayer) => ({
+  //             docId: request.docId,
+  //             uid: prayer.uid,
+  //             notificationTimestamp: prayer.time,
+  //             type: "requests",
+  //           }))
+  //       );
 
-        if (newNotifications.length > 0) {
-          if (audioRef.current) {
-            audioRef.current.play();
-          }
-        }
+  //       if (newNotifications.length > 0 && audioRef.current) {
+  //         audioRef.current.play();
+  //       }
 
+  //       setNotifications((prev) => {
+  //         const updatedNotifications = [
+  //           ...prev.filter((n) => n.type !== "requests"),
+  //           ...newNotifications,
+  //         ];
+  //         setNewNotificationsCount(newNotifications.length);
+  //         return updatedNotifications;
+  //       });
+  //     });
+  //   }
+  // };
 
-        setNotifications((prev) => {
-          const updatedNotifications = [
-            ...prev.filter((n) => n.type !== "requests"),
-            ...newNotifications,
-          ];
-          setNewNotificationsCount(newNotifications.length);
-          return updatedNotifications;
-        });
-      });
+  // const unsubscribeSnapshot = () => {
+  //   if (unsubscribeRef.current) {
+  //     unsubscribeRef.current();
+  //     unsubscribeRef.current = null;
+  //   }
+  // };
 
-      return () => {
-        unsubscribeRequests();
-      };
-    }
-  }, [user]);
+  // useEffect(() => {
+  //   const visibilityChangeHandler = () => {
+  //     if (document.visibilityState === "visible") {
+  //       // Tab is visible, start listening for changes
+  //       handleSnapshot();
+  //     } else {
+  //       // Tab is hidden, unsubscribe to avoid unnecessary reads
+  //       unsubscribeSnapshot();
+  //     }
+  //   };
+
+  //   // Initial listener setup
+  //   if (document.visibilityState === "visible") {
+  //     handleSnapshot();
+  //   }
+
+  //   // Add event listener for visibility changes
+  //   document.addEventListener("visibilitychange", visibilityChangeHandler);
+
+  //   // Cleanup
+  //   return () => {
+  //     document.removeEventListener("visibilitychange", visibilityChangeHandler);
+  //     unsubscribeSnapshot();
+  //   };
+  // }, [user]);
 
   return (
     <NotificationsContext.Provider
       value={{ newNotificationsCount, setNewNotificationsCount, notifications }}
     >
       {children}
-      <audio ref={audioRef} src="/pop.mp3"  />
+      <audio ref={audioRef} src="/pop.mp3" />
     </NotificationsContext.Provider>
   );
 };
